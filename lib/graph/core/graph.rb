61876df728267be96af83113c193d88fc0bc7434
allanborba <allan.borba@prevision.com.br>
2022-12-17 16:23:10 -0300

    adjacency matrix communication with graph class

--- lib/graph/core/graph.rb
+++ lib/graph/core/graph.rb
@@ -1,5 +1,5 @@
 class Graph
-  attr_reader :vertices, :indexed_label
+  attr_reader :vertices, :index_by_label
 
   def initialize(max_vertices_allowed = 10)
     raise ArgumentError.new('Max vertices must be a number') unless max_vertices_allowed.instance_of?(Integer)
@@ -8,14 +8,40 @@ class Graph
     @max_vertices_allowed = max_vertices_allowed
     @vertices = []
     @vertex_index = 0
-    @indexed_label = Hash.new { |h, k| h[k] = @vertex_index } # allow to get index of a vertice in O(1) time
+    @index_by_label = {} # allow to get index of a vertex in O(1) time
   end
 
   def add_vertex(label)
     raise ArgumentError.new('Max allowed vertices has been exceeded') if vertices.size == @max_vertices_allowed
 
-    indexed_label[label]
+    index_by_label[label] = @vertex_index
     vertices << Vertex.new(label)
     @vertex_index += 1
+  end
+
+  def connect_vertex_by_label(initial, final)
+    raise ArgumentError.new('Label must be a String') unless initial.instance_of?(String) && final.instance_of?(String)
+
+    validate_label_existence(initial, final)
+
+    initial_vertex_index = index_by_label[initial]
+    final_vertex_index = index_by_label[final]
+    adjacency_matrix.add_edge(initial_vertex_index, final_vertex_index)
+  end
+
+  def get_adjacencies(label)
+    raise ArgumentError.new('Label must exist') if index_by_label[label].nil?
+
+    adjacency_matrix.get_adjacencies(index_by_label[label])
+  end
+
+  private
+
+  def validate_label_existence(initial, final)
+    raise ArgumentError.new('Label must exist') if index_by_label[initial].nil? || index_by_label[final].nil?
+  end
+
+  def adjacency_matrix
+    @adjacency_matrix ||= AdjacencyMatrix.new(vertices)
   end
 end
--- spec/graph/core/graph_spec.rb
+++ spec/graph/core/graph_spec.rb
@@ -13,18 +13,71 @@ describe Graph do
   end
 
   describe '#add_vertex' do
+    context 'when the number of vertices exceed the max allowed' do
     let(:graph) {described_class.new(1)}
 
-
-    context 'when the number of vertices exceed the max allowed' do
-      before {graph.add_vertex('A')}
+    before {graph.add_vertex('A')}
 
       it {expect{graph.add_vertex('B')}.to raise_error(ArgumentError, 'Max allowed vertices has been exceeded')}
     end
 
     context 'when the number of vertices is valid' do
-      it('incress the vertices size') {expect{graph.add_vertex('A')}.to change(graph.vertices, :count).by(1)}
-      it('get the index by label') {expect(graph.indexed_label['A']).to eq(0)}
+      let(:graph) {described_class.new(2)}
+
+      before {graph.add_vertex('A')}
+
+      it('get the index by label') {expect(graph.index_by_label['A']).to eq(0)}
+      it('incress the vertices size') {expect{graph.add_vertex('B')}.to change(graph.vertices, :count).by(1)}
+    end
+  end
+
+  describe '#connect_vertex_by_label' do
+    let(:adjacency_matrix_mock) {instance_double(AdjacencyMatrix)}
+    let(:graph) {described_class.new}
+    let(:index_of_vertex_a) {0}
+    let(:index_of_vertex_b) {1}
+
+    before do
+      allow(AdjacencyMatrix).to receive(:new).and_return(adjacency_matrix_mock)
+      allow(adjacency_matrix_mock).to receive(:add_edge)
+      graph.add_vertex('A')
+      graph.add_vertex('B')
+    end
+
+    context "when the label is valid" do
+      before {graph.connect_vertex_by_label('A', 'B')}
+
+      it "call matrix add_adge method" do
+        expect(adjacency_matrix_mock).to have_received(:add_edge).with(index_of_vertex_a, index_of_vertex_b)
+      end
+    end
+
+    context "when the label is invalid" do
+      it {expect {graph.connect_vertex_by_label('A', 'C')}.to raise_error(ArgumentError, 'Label must exist')}
+    end
+  end
+
+  describe '#get_adjcencies' do
+    let(:adjacency_matrix_mock) {instance_double(AdjacencyMatrix)}
+    let(:graph) {described_class.new}
+    let(:index_of_vertex) {0}
+
+    before do
+      allow(AdjacencyMatrix).to receive(:new).and_return(adjacency_matrix_mock)
+      allow(adjacency_matrix_mock).to receive(:get_adjacencies)
+      graph.add_vertex('A')
+    end
+
+    context "when the label is" do
+      before {graph.get_adjacencies('A')}
+
+      it "call adjacency matrix get_adjacencies method" do
+        expect(adjacency_matrix_mock).to have_received(:get_adjacencies).with(index_of_vertex)
+      end
+    end
+
+    context "when the label is invalid" do
+      it {expect {graph.get_adjacencies('C')}.to raise_error(ArgumentError, 'Label must exist')}
     end
   end
 end
