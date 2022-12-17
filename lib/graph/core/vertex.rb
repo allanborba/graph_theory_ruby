46fc1dffbef70e21e5b02135e238f0068e96dacf
allanborba <allan.borba@prevision.com.br>
2022-12-17 16:22:09 -0300

    add degree to vertex

--- lib/graph/core/vertex.rb
+++ lib/graph/core/vertex.rb
@@ -1,10 +1,15 @@
 class Vertex
-  attr_reader :label
+  attr_reader :label, :degree
 
   def initialize(label)
     raise ArgumentError.new('Label must be a String') unless label.instance_of?(String)
     raise ArgumentError.new('Blank label not allowed') if label.strip == ''
 
     @label = label
+    @degree = 0
+  end
+
+  def add_degree
+    @degree += 1
   end
 end
