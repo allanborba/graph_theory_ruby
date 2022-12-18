465d1546f120890bb9220687214d7ffc60185aae
allanborba <allan.borba@prevision.com.br>
2022-12-18 09:52:09 -0300

    breadth first search

---
+++ lib/domain/search/breadth_first_search .rb
@@ -0,0 +1,35 @@
+require "singleton"
+require_relative "path"
+
+class BreadthFristSearch
+  include Singleton
+
+  def search(graph, beginning, destination)
+    raise ArgumentError, "graph must be a Graph type" unless graph.is_a?(Graph)
+
+    graph.validate_label_existence(beginning, destination)
+
+    fifo = []
+    visited_vertices = []
+    path = Path.new
+
+    fifo << beginning
+
+    while fifo.any?
+      vertex = fifo.shift
+      break if vertex == destination
+
+      graph.get_adjacencies(vertex).each do |adjacent_vertex|
+        label = adjacent_vertex.label
+
+        next if visited_vertices.include?(label)
+
+        visited_vertices << label
+        path.connect(label, vertex)
+        fifo << label
+      end
+    end
+
+    path.generate(beginning, destination)
+  end
+end
---
+++ spec/domain/search/breadth_first_search _spec.rb
@@ -0,0 +1,26 @@
+require "domain/search/depth_first_search"
+require_relative "../../graph_helper"
+
+describe DepthFristSearch do
+  describe "#search" do
+    let(:graph) { generate_graph }
+    let(:path_result) { ["A", "D", "G"] }
+
+    context "when graph its not a Graph instance" do
+      it do
+        expect { described_class.instance.search("graph", "A", "G") }
+          .to raise_error(ArgumentError, "graph must be a Graph type")
+      end
+    end
+
+    context "when vertices dont belong to graph" do
+      it do
+        expect { described_class.instance.search(graph, "X", "Z") }.to raise_error(ArgumentError, "Label must exist")
+      end
+    end
+
+    context "when vertices belong to graph" do
+      it("returns the vertices path") { expect(described_class.instance.search(graph, "A", "G")).to eq(path_result) }
+    end
+  end
+end
