ad0adfe131ba17c4e27cbcfa3fb8717885668982
allanborba <allan.borba@prevision.com.br>
2022-12-20 07:53:25 -0300

    add directed and weighted edges and copy values

--- lib/domain/core/adjacency_matrix.rb
+++ lib/domain/core/adjacency_matrix.rb
@@ -1,16 +1,18 @@
 class AdjacencyMatrix
-  attr_reader :vertices, :matrix
+  attr_reader :vertices, :ancesters, :matrix_size, :matrix
 
   def initialize(vertices)
-    raise ArgumentError, "Argument must be a list of vertices" if vertices.any?{|v| !v.instance_of?(Vertex)}
+    raise ArgumentError, "Argument must be a list of vertices" if vertices.any? { |v| !v.instance_of?(Vertex) }
 
     @vertices = vertices
+    @ancesters = {}
+    @matrix_size = vertices.size
     initialize_matrix
   end
 
-  def add_edge(initial_vertex_index, final_vertex_index)
-    raise ArgumentError, "index must be a number" unless initial_vertex_index.instance_of?(Integer) && final_vertex_index.instance_of?(Integer)
-    raise ArgumentError, "index must be inside matrix size" if initial_vertex_index > vertices.size || final_vertex_index > vertices.size
+  def add_edge(initial_vertex_index, final_vertex_index, weight = 1)
+    validate_vertices_index([initial_vertex_index, final_vertex_index])
+    validate_weight(weight)
 
     matrix[initial_vertex_index][final_vertex_index] = 1
     vertices[initial_vertex_index].add_degree
@@ -19,6 +21,16 @@ class AdjacencyMatrix
 
     matrix[final_vertex_index][initial_vertex_index] = 1
     vertices[final_vertex_index].add_degree
+  end
+
+  def add_directed_edge(initial_vertex_index, final_vertex_index, weight = 1)
+    validate_vertices_index([initial_vertex_index, final_vertex_index])
+    validate_weight(weight)
+
+    write_in_matrix(initial_vertex_index, final_vertex_index, weight)
+    vertices[final_vertex_index].add_degree
+
+    ancesters[final_vertex_index] = vertices[initial_vertex_index]
   end
 
   def get_adjacencies(vertex_index)
@@ -31,11 +43,35 @@ class AdjacencyMatrix
     adjacencies
   end
 
+  def copy_matrix_values(destination_matrix)
+    raise ArgumentError, "Argument must be a Matrix" unless destination_matrix.is_a?(AdjacencyMatrix)
+    raise ArgumentError, "Matrix must have same size" unless destination_matrix.matrix_size == matrix_size
+
+    matrix_size.times do |i|
+      matrix_size.times { |j| destination_matrix.write_in_matrix(i, j, matrix[i][j]) }
+    end
+  end
+
   private
 
   def initialize_matrix
-    @matrix = Array.new(vertices.size)
+    @matrix = Array.new(matrix_size)
 
-    vertices.size.times {|i| matrix[i]= Array.new(vertices.size, 0)}
+    matrix_size.times { |i| matrix[i]= Array.new(vertices.size, 0) }
+  end
+
+  def validate_vertices_index(list_of_index)
+    raise ArgumentError, "index must be a number" if list_of_index.any? { |i| !i.instance_of?(Integer) }
+    raise ArgumentError, "index must be inside matrix size" if list_of_index.any? { |i| i > vertices.size }
+  end
+
+  def validate_weight(weight)
+    raise ArgumentError, "weight must be a number" if weight.instance_of?(Integer)
+  end
+
+  protected
+
+  def write_in_matrix(line, column, weight)
+    matrix[line][column] = weight
   end
 end
--- spec/domain/core/adjacency_matrix_spec.rb
+++ spec/domain/core/adjacency_matrix_spec.rb
@@ -1,45 +1,90 @@
 require "domain/core/adjacency_matrix"
 
 describe AdjacencyMatrix do
-  let(:vertex_A) {Vertex.new("A")}
-  let(:vertex_B) {Vertex.new("B")}
-  let(:vertex_C) {Vertex.new("C")}
-  let(:vertex_D) {Vertex.new("D")}
-  let(:vertices) {[vertex_A, vertex_B, vertex_C, vertex_D]}
+  let(:vertex_A) { Vertex.new("A") }
+  let(:vertex_B) { Vertex.new("B") }
+  let(:vertex_C) { Vertex.new("C") }
+  let(:vertex_D) { Vertex.new("D") }
+  let(:vertices) { [vertex_A, vertex_B, vertex_C, vertex_D] }
 
   describe "#initialize" do
     context "when the argument is not a list of vectices" do
-      let(:wrong_args) {["A", "B", "C", "D"]}
+      let(:wrong_args) { ["A", "B", "C", "D"] }
 
-      it {expect{described_class.new(wrong_args)}.to raise_error(ArgumentError, "Argument must be a list of vertices")}
+      it do
+        expect { described_class.new(wrong_args) }.to raise_error(ArgumentError, "Argument must be a list of vertices")
+      end
     end
 
     context "when the argument is valid" do
-      let(:adjacency_matrix) {described_class.new(vertices)}
-      let(:result_matrix) { [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]}
+      let(:adjacency_matrix) { described_class.new(vertices) }
+      let(:result_matrix) { [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]] }
 
-      it("initialize the matrix") {expect(adjacency_matrix.matrix).to eq(result_matrix)}
+      it("initialize the matrix") { expect(adjacency_matrix.matrix).to eq(result_matrix) }
     end
   end
 
   describe "#add_edge" do
-    let(:adjacency_matrix) {described_class.new(vertices)}
-    let(:result_matrix) { [[0,0,0,0], [0,0,0,1], [0,0,0,0], [0,1,0,0]]}
+    let(:adjacency_matrix) { described_class.new(vertices) }
+    let(:result_matrix) { [[0, 0, 0, 0], [0, 0, 0, 1], [0, 0, 0, 0], [0, 1, 0, 0]] }
 
-    before {adjacency_matrix.add_edge(1, 3)}
+    before { adjacency_matrix.add_edge(1, 3) }
 
     context "when add edge" do
-      it("change the value of matrix") {expect(adjacency_matrix.matrix).to eq(result_matrix)}
-      it("change the dree of each vertex") {expect(vertices.map(&:degree)).to eq([0,1,0,1])}
+      it("change the value of matrix") { expect(adjacency_matrix.matrix).to eq(result_matrix) }
+      it("change the degree of each vertex") { expect(vertices.map(&:degree)).to eq([0, 1, 0, 1]) }
+    end
+  end
+
+  describe "#add_directed_edge" do
+    let(:adjacency_matrix) { described_class.new(vertices) }
+    let(:result_matrix) { [[0, 0, 0, 0], [0, 0, 0, 5], [0, 0, 0, 0], [0, 0, 0, 0]] }
+
+    before { adjacency_matrix.add_directed_edge(1, 3, 5) }
+
+    context "when add edge" do
+      it("change the value of matrix") { expect(adjacency_matrix.matrix).to eq(result_matrix) }
+      it("change the degree of each vertex") { expect(vertices.map(&:degree)).to eq([0, 0, 0, 1]) }
+    end
+  end
+
+  describe "#copy_matrix_values" do
+    let(:adjacency_matrix) { described_class.new(vertices) }
+
+    context "when the argument isnt a matrix" do
+      it do
+        expect { adjacency_matrix.copy_matrix_values("matrix") }
+          .to raise_error(ArgumentError, "Argument must be a Matrix")
+      end
+    end
+
+    context "when the matrix has diff sizes" do
+      let(:new_matrix) { described_class.new([vertex_A, vertex_B]) }
+
+      it do
+        expect { adjacency_matrix.copy_matrix_values(new_matrix) }
+          .to raise_error(ArgumentError, "Matrix must have same size")
+      end
+    end
+
+    context "when receive valid matrix as argument" do
+      let(:new_matrix) { described_class.new(vertices) }
+      let(:result_matrix) { [[0, 0, 0, 0], [0, 0, 0, 1], [0, 0, 0, 0], [0, 1, 0, 0]] }
+
+      before do
+        adjacency_matrix.add_edge(1, 3)
+        adjacency_matrix.copy_matrix_values(new_matrix)
+      end
+
+      it("to copy the values") { expect(new_matrix.matrix).to eq(result_matrix) }
     end
   end
 
   describe "#get_adjacencies" do
-    let(:adjacency_matrix) {described_class.new(vertices)}
+    let(:adjacency_matrix) { described_class.new(vertices) }
 
-    before {adjacency_matrix.add_edge(1, 3)}
+    before { adjacency_matrix.add_edge(1, 3) }
 
-    it("returns the adjacencies") {expect(adjacency_matrix.get_adjacencies(1)).to eq([vertex_D])}
-
+    it("returns the adjacencies") { expect(adjacency_matrix.get_adjacencies(1)).to eq([vertex_D]) }
   end
 end
